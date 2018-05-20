8dc94d5397ede980f815d61506bd52ebe9ae4a47
Arindam <asarma@ucsc.edu>
2018-05-19 22:38:24 -0700

    Updated colored region detection script, verified that it works, however there is a pretty nasty inefficiency issue that needs to be resolved.

--- scripts/colored_region_detection/c_r_d.py
+++ scripts/colored_region_detection/c_r_d.py
@@ -4,14 +4,17 @@ import numpy as np
 import cv2
 import math
 import argparse
+import time
 
 
 image_folder = './test_images'
+min_region_size = 14400
 
 # rect = ((min_x, min_y), (max_x, max_y))
 def place_bounding_box(image, rect):
-	min_x, max_x, min_y, max_y = rect
-	# ...
+	image = cv2.rectangle(image,(rect[0][1],rect[0][0]),(rect[1][1],rect[1][0]),(0,255,0),3)
+	return image
+
 	
 def calc_bounding_box(coord_list):
 	min_x = min(coord_list, key = lambda t: t[0])[0]
@@ -46,7 +49,7 @@ def scan_image(image):
 					coord_list.append((row,col))
 					to_visit = []
 					for direction in directions:
-						new_loc = (row,col)+direction
+						new_loc = (row + direction[0],col + direction[1])
 						if not new_loc[0] < 0 and not new_loc[0] > (rows-1) and not new_loc[1] < 0 and not new_loc[1] > (cols-1):
 							if not visited_matrix[new_loc[0]][new_loc[1]]:
 								to_visit.append(new_loc)
@@ -57,14 +60,16 @@ def scan_image(image):
 						if not np.array_equal(image[current_loc[0],current_loc[1]], [0,0,0]):
 							coord_list.append(current_loc)
 							for direction in directions:
-								new_loc = (current_loc[0], current_loc[1])+direction
+								new_loc = (current_loc[0] + direction[0], current_loc[1] + direction[1])
 								if not new_loc[0] < 0 and not new_loc[0] > (rows-1) and not new_loc[1] < 0 and not new_loc[1] > (cols-1):
 									if not visited_matrix[new_loc[0]][new_loc[1]]:
 										to_visit.append(new_loc)
-					region_list.append(coord_list)
+					if len(coord_list) > min_region_size:
+						region_list.append(coord_list)
 
 	return region_list
 
+start = time.time()
 
 file_names = os.listdir(image_folder)
 
@@ -73,39 +78,29 @@ image_paths = []
 for file in file_names:
 	image_paths.append(os.path.join(image_folder, file))
 
-images = []
+output = []
 
 for image_path in image_paths:
-	images.append(cv2.imread(image_path, 1))
+	image = cv2.imread(image_path, 1)
+	region_list = scan_image(image)
+	if region_list:
+		rects = []
+		for region in region_list:
+			rects.append(calc_bounding_box(region))
+		image_with_box = image.copy()
+		for rect in rects:
+			place_bounding_box(image_with_box, rect)
+		output.append(image_with_box)
+		
+end = time.time()
+
+for index, image in enumerate(output):
+	cv2.imshow(('Output ' + str(index)), image)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
 
 
-region_list = scan_image(images[0])
-
-rects = []
-
-for region in region_list:
-	rects.append(calc_bounding_box(region))
-
-
-'''
-for rect in rects:
-	place_bounding_box()
-'''
-
-print len(region_list)
-
-print len(rects)
-
-
-
-
-'''
-for image, name in zip(images, file_names):
-	cv2.imshow(name, image)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-print image_paths
-'''
+total = end-start
+print ('Total: ' + str(total))
+print ('Time per image: ' + str(total/len(image_paths)))
 
