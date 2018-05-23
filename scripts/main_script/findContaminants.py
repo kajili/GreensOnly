ff57a0cddb750c13f49da56e18d8010480f03165
Cesar Neri <ceneri@ucsc.edu>
2018-05-22 17:41:27 -0700

    Main script now makes use of flagContaminant and c_r_d methods to flag and highlight a contaminant found. Main script now takes two more optional arguments to specify the percentage of white needed to flag a potential image, and the pixel minimum pixel size of a contaminant to be highlighted.

--- scripts/main_script/findContaminants.py
+++ scripts/main_script/findContaminants.py
@@ -1,18 +1,27 @@
 #!/usr/env/bin python3
 
 """
-" File:     findContaminants.py
-" Date:     05-10-2018
+" File:     findContaminantsV2.py
+" Date:     05-21-2018
 """
 
 import os
 import cv2
-import random ##!!For now
-#import fnmatch
 import argparse
 import numpy as np
 import tkinter as tk
+from PIL import Image
+from matplotlib import pyplot as plt
 
+DEFAULT_WHITE_PERCENTAGE = 99.4
+DEFAULT_PIXEL_SIZE = 10000
+DEFAULT_OUTPUT_DIRECTORY = "ContaminantOutput"
+
+def turnGreenLight():
+    return cv2.imread("green_light_icon.png")
+
+def turnRedLight():
+    return cv2.imread("red_light_icon.png")
 
 def parseArguments():
     """
@@ -23,10 +32,15 @@ def parseArguments():
 
     parser.add_argument('-v', '--video', required=True,
                         help="Path to video.")
-    parser.add_argument('-o', '--directory', required=False, default="ContaminantOutput",
+    parser.add_argument('-o', '--directory', required=False, default=DEFAULT_OUTPUT_DIRECTORY,
                         help="Path to directory where output images created should be stored.")
     parser.add_argument('-b', '--bMask', required=False, default=None,
                         help="Path to background mask (image).")
+    parser.add_argument('-w', '--white', required=False, default=DEFAULT_WHITE_PERCENTAGE, type=float,
+                        help="Percent value of white to be used when evaluating potential for contaminant.")
+    parser.add_argument('-p', '--pixels', required=False, default=DEFAULT_PIXEL_SIZE, type=int,
+                        help="""Minimum pixel size of a region that should be identified as contaminant, 
+                        and highlighted inside rectangular box.""")
     parser.add_argument('-l', '--long', required=False, default=False, action='store_true', 
                         help="Long mode shows masking of every frame as it goes.")
     
@@ -36,7 +50,9 @@ def parseArguments():
     outputDir = args['directory']
     backgroundMask = args['bMask']
     longFlag = args['long']
-    return videoP, outputDir, backgroundMask, longFlag 
+    whiteFlag = args['white']
+    minPixls = args['pixels']
+    return videoP, outputDir, backgroundMask, longFlag, whiteFlag, minPixls
 
 def resizeWindows():
     """
@@ -56,25 +72,24 @@ def resizeWindows():
     cv2.namedWindow('original', cv2.WINDOW_NORMAL)
     cv2.namedWindow('background_strip', cv2.WINDOW_NORMAL)
     cv2.namedWindow('green_strip', cv2.WINDOW_NORMAL)
-    cv2.namedWindow('green_only', cv2.WINDOW_NORMAL)
-    cv2.namedWindow('color_palette', cv2.WINDOW_NORMAL)
-    cv2.namedWindow('contaminants', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('flag_contaminant', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('highlight_contaminant', cv2.WINDOW_NORMAL)
 
     cv2.moveWindow("original", 0, 30);
     cv2.moveWindow("background_strip", mid_width, 30);
     cv2.moveWindow("green_strip", mid_width*2, 30);
-    cv2.moveWindow("green_only", 0, mid_height+30);
-    cv2.moveWindow("color_palette", mid_width, mid_height+30);
-    cv2.moveWindow("contaminants", mid_width*2, mid_height+30);
+    cv2.moveWindow("flag_contaminant", 0, mid_height+30);
+    cv2.moveWindow("highlight_contaminant", mid_width, mid_height+30);
+    #cv2.moveWindow("contaminants", mid_width*2, mid_height+30);
 
     cv2.resizeWindow('original', mid_width, mid_height)
     cv2.resizeWindow('background_strip', mid_width, mid_height)
     cv2.resizeWindow('green_strip', mid_width, mid_height)
-    cv2.resizeWindow('green_only', mid_width, mid_height)
-    cv2.resizeWindow('color_palette', mid_width, mid_height)
-    cv2.resizeWindow('contaminants', mid_width, mid_height)
+    cv2.resizeWindow('flag_contaminant', mid_width, mid_height)
+    cv2.resizeWindow('highlight_contaminant', mid_width, mid_height)
+    #cv2.resizeWindow('contaminants', mid_width, mid_height)
 
-def processDisplay(original, backgroundStrip, greenStrip, greenOnly):
+def processDisplay(original, backgroundStrip, greenStrip, flagContam, highlContam):
     """
     Updates and displays ORIGINAL, BACKGROUNDSTRIP and GREENSTRIP images/frames
     in their corresponding windows
@@ -82,7 +97,8 @@ def processDisplay(original, backgroundStrip, greenStrip, greenOnly):
     cv2.imshow('original', original)
     cv2.imshow('background_strip', backgroundStrip)           
     cv2.imshow('green_strip', greenStrip)
-    cv2.imshow('green_only', greenOnly) 
+    cv2.imshow('flag_contaminant', flagContam)
+    cv2.imshow('highlight_contaminant', highlContam)
 
 def maskBackground(bMask, frame):
     """
@@ -127,22 +143,137 @@ def createBackgroundMask():
     print("Need to create Mask")
     return "lel"
 
-def dummyColorPalette(frame):
-    """
-    !!TEMPORARY FUNCTION
-    Should return image with fixed color palette
-    """
-    return frame
+def applyThresholding(frame):
 
-def dummyContaminantImage(frame, dummyCount):
+    TEMP_IMG_NAME = "temp.jpg"
+
+    image = Image.fromarray(frame)
+    # Save image using PIL due to previous errors with adaptiveThreshold only working with imread
+    image.save(TEMP_IMG_NAME)
+
+    # Apply Gaussian Thresholding to image
+    imgBeforeThresh = cv2.imread("temp.jpg",0)
+    imgBeforeThresh = cv2.medianBlur(imgBeforeThresh,5)
+    imgGauss = cv2.adaptiveThreshold(imgBeforeThresh,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                cv2.THRESH_BINARY,11,2)
+
+    # Remove temporary image used for processing
+    os.remove(TEMP_IMG_NAME)
+    return imgGauss
+
+def findContaminant(frame, whiteFlag):
     """
-    !!TEMPORARY FUNCTION
-    Real function should only take FRAME argument, and return 
+    Takes FRAME argument, and returns
     image highlighting contaminants, and boolean if contaminant found
     """
-    contaminantFrames = [38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 100]
 
-    return frame, dummyCount in contaminantFrames
+    thresh = applyThresholding(frame)
+
+    # Convert thresh array into image for use with PIL libraries
+    image = Image.fromarray(thresh)
+
+    #getcolors() gets a list of rgb in an image. return a list of (count, pixel)
+    colors = image.convert('RGB').getcolors(100000)
+
+    sortedColors = sorted(colors)
+
+    #Percent from the number of times a certain color pixel appears over all the pixels in an image
+    totalPixels = 0
+    whitePixels = 0
+    for x in sortedColors:
+        totalPixels = totalPixels + x[0] 
+        if(235 < x[1][0] and 235 < x[1][1] and 235 < x[1][2]):  
+            whitePixels = whitePixels + x[0]
+
+    percentOfWhite = (whitePixels / totalPixels) * 100
+
+    contaminantIsFound = False
+
+    # If image has less than 99.40% white pixels, it's very likely to contain a contaminant.
+    #  Note: This is based on testing images after masking green + background,
+    #  where 99.45% is the lowest value found in images with no contaminants. Subject to change. 
+    if(percentOfWhite < whiteFlag):
+        # Image is flagged as having a contaminant if it has less than 99.40% white pixels
+        contaminantIsFound = True
+        # # Print the percentage of white of the passing frames, for testing
+        # print(percentOfWhite)
+
+    if contaminantIsFound:
+        return turnRedLight(), contaminantIsFound
+    else:
+        return turnGreenLight(), contaminantIsFound
+
+#******Methods from c_r_d.py*******
+
+# rect = ((min_x, min_y), (max_x, max_y))
+def place_bounding_box(image, rect):
+    image = cv2.rectangle(image,(rect[0][1],rect[0][0]),(rect[1][1],rect[1][0]),(0,255,0),3)
+    return image
+
+def calc_bounding_box(coord_list):
+    min_x = min(coord_list, key = lambda t: t[0])[0]
+    min_y = min(coord_list, key = lambda t: t[1])[1]
+    max_x = max(coord_list, key = lambda t: t[0])[0]
+    max_y = max(coord_list, key = lambda t: t[1])[1]
+    rect = ((min_x, min_y),(max_x, max_y))
+
+    return rect
+
+def scan_image(image, min_region_size):
+    region_list = []
+
+    rows, cols, channels = image.shape
+    visited_matrix = [[False for col in range(cols)] for row in range(rows)]
+    
+    directions = [(-1,-1), (-1, 0), (-1, 1), (0,-1), (0, 1), (1,-1), (1, 0), (1, 1)]
+
+    for row in range(rows):
+        for col in range(cols):
+            # top level linear scan
+            if not visited_matrix[row][col]:
+                visited_matrix[row][col] = True
+                region = not np.array_equal(image[row,col], [0,0,0])
+                # first step of flood fill
+                if region:
+                    coord_list = []
+                    coord_list.append((row,col))
+                    to_visit = []
+                    for direction in directions:
+                        new_loc = (row + direction[0],col + direction[1])
+                        if not new_loc[0] < 0 and not new_loc[0] > (rows-1) and not new_loc[1] < 0 and not new_loc[1] > (cols-1):
+                            if not visited_matrix[new_loc[0]][new_loc[1]]:
+                                to_visit.append(new_loc)
+                    #start flood fill
+                    while to_visit:
+                        current_loc = to_visit.pop()
+                        visited_matrix[current_loc[0]][current_loc[1]] = True
+                        if not np.array_equal(image[current_loc[0],current_loc[1]], [0,0,0]):
+                            coord_list.append(current_loc)
+                            for direction in directions:
+                                new_loc = (current_loc[0] + direction[0], current_loc[1] + direction[1])
+                                if not new_loc[0] < 0 and not new_loc[0] > (rows-1) and not new_loc[1] < 0 and not new_loc[1] > (cols-1):
+                                    if not visited_matrix[new_loc[0]][new_loc[1]]:
+                                        to_visit.append(new_loc)
+                    if len(coord_list) > min_region_size:
+                        region_list.append(coord_list)
+
+    return region_list
+
+def highlightContaminant(flaggedImg, minRegionSize):
+
+    #image = cv2.imread(imgPath, 1)
+    region_list = scan_image(flaggedImg, minRegionSize)
+    if region_list:
+        rects = []
+        for region in region_list:
+            rects.append(calc_bounding_box(region))
+        image_with_box = flaggedImg.copy()
+        for rect in rects:
+            place_bounding_box(image_with_box, rect)
+            
+        return image_with_box, True
+
+    return turnGreenLight(), False
 
 def getTimeStamp(capture):
     """
@@ -154,10 +285,11 @@ def getTimeStamp(capture):
 
     return timestamp.replace('.', '_') + "_sec"
 
+
 def main():
 
     #Arguments
-    videoPath, outputDirPath, bckgdMaskPath, longFlag = parseArguments()
+    videoPath, outputDirPath, bckgdMaskPath, longFlag, whitePercentFlag, minPixelSize = parseArguments()
     if bckgdMaskPath == None:
         bckgdMaskPath = createBackgroundMask()
 
@@ -166,11 +298,11 @@ def main():
     bMask = cv2.imread(bckgdMaskPath)
     videoCapture = cv2.VideoCapture(videoPath)
 
+    #Image declaration, for faster calling
+    greenLight = turnGreenLight()
+
     if longFlag:
         resizeWindows()
-
-    #!!FOR NOW ONLY!!!
-    dummyCount = 0
 
     #Go trough every frame
     while True:
@@ -184,22 +316,26 @@ def main():
         backgroundStrip = maskBackground(bMask, frame)
         #Mask color green
         greensOnly, greenStrip = maskGreen(backgroundStrip)
-        #!!Simplify color palette
-        colorPalette = dummyColorPalette(greenStrip)
-        #!!Identify large chunks of the same color
-        contaminantImg, isFound = dummyContaminantImage(colorPalette, dummyCount)
+        #Check for possible contaminant
+        contaminantImg, isFoundFirstPass = findContaminant(greenStrip, whitePercentFlag)
+        #
+        highlightImg = greenLight
 
-        #If image is flagged, save img with timestamp
-        if isFound:
-            timestamp = getTimeStamp(videoCapture)
-            cv2.imwrite(outputDirPath + '/' +  str(timestamp) + ".jpg", contaminantImg)
+        #If image is flagged
+        if isFoundFirstPass:
 
-        #!!FOR NOW ONLY!!!
-        dummyCount += 1
+            #Check to highlight contaminant
+            highlightImg, isFoundSecondPass = highlightContaminant(greenStrip, minPixelSize)
+
+            #If highlight works
+            if isFoundSecondPass:
+
+                timestamp = getTimeStamp(videoCapture)
+                cv2.imwrite(outputDirPath + '/' +  str(timestamp) + ".jpg", highlightImg)
 
         #Display different masked frames
         if longFlag:
-            processDisplay(frame, backgroundStrip, greenStrip, greensOnly)
+            processDisplay(frame, backgroundStrip, greenStrip, contaminantImg, highlightImg)
 
             k = cv2.waitKey(1) & 0xff
             if k == 27:
